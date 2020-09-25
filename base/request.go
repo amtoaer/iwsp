35aa79d9b309acba3e9b3c47950d492e3c4382f2
amtoaer <amtoaer@outlook.com>
2020-09-25 11:53:50 +0800

    chore: 对Post函数进行小改动

--- base/request.go
+++ base/request.go
@@ -1,12 +1,12 @@
 package base
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"iwsp/utils"
 	"net/http"
-	"strings"
 )
 
 // GetInfo 得到姓名与场次信息（待具体实现）
@@ -23,20 +23,20 @@ func (s *Session) GetInfo() (content string, err error) {
 
 // Post post预约信息
 func (s *Session) Post() {
-	result, err := json.Marshal(s.data)
+	data, err := json.Marshal(s.data)
 	utils.Log("序列化预约信息：", s.data)
 	if err != nil {
 		utils.Fatal("data序列化失败，请重试")
 	}
-	utils.Log("序列化结果:", string(result))
+	utils.Log("序列化结果:", string(data))
 	utils.Log("开始进行post请求...")
-	resp, err := s.client.Post(s.createURL, "application/json;charset=UTF-8", strings.NewReader(string(result)))
+	resp, err := s.client.Post(s.createURL, "application/json;charset=UTF-8", bytes.NewBuffer(data))
 	if err != nil {
 		utils.Fatal("post预约请求失败，请重试")
 	}
-	defer resp.Body.Close()
 	fmt.Println(resp.Status)
-	utils.Log("预约返回结果：\n", resp.Body)
+	result, err := readBody(resp)
+	utils.Log("预约返回结果：\n", result)
 
 }
 
